0cfb75b479ff19023a466c531a9d6b0a40ccb2a4
ISIKALU01 <davisleevtobi@gmail.com>
2022-11-16 16:51:58 +0100

    Add basic game Ai

--- script.js
+++ script.js
@@ -10,15 +10,14 @@ const Player = (sign) => {
 
 
 
-
-
-
 const displayController = (() => {
     const fieldElements = document.querySelectorAll(".field");
     const restartButton = document.querySelector(".restart-button");
+
     
     fieldElements.forEach((field) =>
       field.addEventListener("click", (e) => {
+        if(field.textContent)return;
         gameController.playRound(parseInt(e.target.dataset.index));
         updateGameboard();
       })
@@ -27,12 +26,12 @@ const displayController = (() => {
     restartButton.addEventListener("click", (e) => {
       gameBoard.reset();
       gameController.reset();
-      updateGameboard();
+      updateGameboard();   
     });
   
     const updateGameboard = () => {
       for (let i = 0; i < fieldElements.length; i++) {
-        fieldElements[i].textContent = gameBoard.getField(i);
+        fieldElements[i].textContent = gameBoard.getField(i);  
       }
     };
   })();
@@ -40,32 +39,42 @@ const displayController = (() => {
 
 
 
-  const gameController = (() => {
-    const playerX = Player("X");
-    const playerO = Player("O");
-    let round = 0;
+
+
+
+const gameController = (() => {
+  const _huPlayer = Player("X");
+  const _aiPlayer = Player("O");
+
+  const getHumanPlayer = () => _huPlayer;
+  const getAiPlayer = () => _aiPlayer;
+
+
+  let round = 0;
 
   
+  const playRound = (fieldIndex) => {
+    gameBoard.setField(fieldIndex, getHumanPlayer().getSign()); 
+    bestSpot()
+    round++;
+  };
+  
+  const getIsOver = () => {
+    return isOver;
+  };
+  
+  const reset = () => {
+    round = 0;
+    isOver = false;
+  };
 
-    const playRound = (fieldIndex) => {
-      gameBoard.setField(fieldIndex, getCurrentPlayerSign());
-      round++;
-    };
+  const bestSpot = () => {
+    const emptyCells = gameBoard.getEmptyFields()
+    gameBoard.setField(emptyCells[0], getAiPlayer().getSign())  
+  }
   
-    const getCurrentPlayerSign = () => {
-      return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
-    };
   
-    const getIsOver = () => {
-      return isOver;
-    };
-  
-    const reset = () => {
-      round = 1;
-      isOver = false;
-    };
-  
-    return { playRound, getIsOver, reset };
+    return { playRound, getIsOver, reset, bestSpot };
   })();
 
 
@@ -74,8 +83,11 @@ const displayController = (() => {
 
 
 
-  const gameBoard = (() => {
+
+
+const gameBoard = (() => {
   const _board = new Array(9);
+  //const _board = ["", "", "", "", "", "", "", "", ""];
 
   const setField = (index, sign) => {
     if (index > _board.length) return;
@@ -84,16 +96,30 @@ const displayController = (() => {
 
   const getField = (index) => {
     if (index > _board.length) return;
+    console.log(_board)
     return _board[index];
   };
 
+  const getEmptyFields = () => {
+    fields = [];
+    for (let i = 0; i < _board.length; i++) {
+        const field = _board[i];
+        if (field == undefined) {
+            fields.push(i);
+        }
+    }
+    return fields;
+  }
+
   const reset = () => {
     for (let i = 0; i < _board.length; i++) {
-      _board[i] = "";
+      _board[i] = undefined;
     }
   };
 
-  return { setField, getField, reset };
+  return { setField, getField, reset, getEmptyFields};
 })();
+
+
 
 
